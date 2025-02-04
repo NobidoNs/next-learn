9c032e4e4009640b429380703cee83725ac807c5
NobidoNs <61043287+NobidoNs@users.noreply.github.com>
2025-02-04 18:17:42 +0500

    test

--- app/api/players/route.ts
+++ app/api/players/route.ts
@@ -1,21 +1,24 @@
 import Redis from 'ioredis'
 import type { User } from '@/app/lib/definitions'
-import { createClient } from '@vercel/postgres'
+// import { createClient } from '@vercel/postgres'
+import { sql } from '@vercel/postgres'
 
 async function getPlayers(): Promise<User[] | undefined> {
-	const client = createClient()
+	// const client = createClient()
 	console.log(1)
-	await client.connect()
+	// await client.connect()
 	console.log(2)
 	try {
+		// const data =
+		// await client.sql<User>`SELECT name, score, rank FROM users ORDER BY score DESC;`
 		const data =
-			await client.sql<User>`SELECT name, score, rank FROM users ORDER BY score DESC;`
+			await sql`SELECT name, score, rank FROM users ORDER BY score DESC;`
 		console.log(3)
-		return data.rows
+		return data.rows as User[]
 	} catch (error) {
 		console.error('Failed to fetch user:', error)
 	} finally {
-		await client.end()
+		// await client.end()
 	}
 }
 
