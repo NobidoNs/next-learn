265ab251b06294b0c702c4719d288352d3251921
NobidoNs <61043287+NobidoNs@users.noreply.github.com>
2025-02-04 18:22:16 +0500

    Revert "test"
    This reverts commit 9c032e4e4009640b429380703cee83725ac807c5.

--- app/api/players/route.ts
+++ app/api/players/route.ts
@@ -1,24 +1,21 @@
 import Redis from 'ioredis'
 import type { User } from '@/app/lib/definitions'
-// import { createClient } from '@vercel/postgres'
-import { sql } from '@vercel/postgres'
+import { createClient } from '@vercel/postgres'
 
 async function getPlayers(): Promise<User[] | undefined> {
-	// const client = createClient()
+	const client = createClient()
 	console.log(1)
-	// await client.connect()
+	await client.connect()
 	console.log(2)
 	try {
-		// const data =
-		// await client.sql<User>`SELECT name, score, rank FROM users ORDER BY score DESC;`
 		const data =
-			await sql`SELECT name, score, rank FROM users ORDER BY score DESC;`
+			await client.sql<User>`SELECT name, score, rank FROM users ORDER BY score DESC;`
 		console.log(3)
-		return data.rows as User[]
+		return data.rows
 	} catch (error) {
 		console.error('Failed to fetch user:', error)
 	} finally {
-		// await client.end()
+		await client.end()
 	}
 }
 
