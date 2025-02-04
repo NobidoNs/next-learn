66701316acb19584e5852864a0a3c18008a49b9d
NobidoNs <61043287+NobidoNs@users.noreply.github.com>
2025-02-04 20:27:41 +0500

    profile fix

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -2,11 +2,13 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function POST() {
-	const cookieStore = cookies()
-	await (await cookieStore).delete('session')
+	cookies().delete('session')
 
 	return NextResponse.json(
 		{ message: 'Signed out successfully' },
 		{ status: 200 }
 	)
+}
+function cookies() {
+	throw new Error('Function not implemented.')
 }
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,14 @@
 export { default } from 'next-auth/middleware'
 import { NextResponse, type NextRequest } from 'next/server'
+import { getSession } from 'next-auth/react'
+import { getToken } from 'next-auth/jwt'
 
-export function middleware(req: NextRequest) {
-	const isLoggedIn = !!req.cookies.has('next-auth.session-token')
+export async function middleware(req: NextRequest) {
+	// const isLoggedIn = !!req.cookies.has('next-auth.session-token')
+	// const session = await getSession({ req: req as any })
+	// const isLoggedIn = await !!session
+	const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+	const isLoggedIn = !!token
 
 	if (req.nextUrl.pathname.startsWith('/login')) {
 		if (isLoggedIn) {
@@ -16,4 +22,7 @@ export function middleware(req: NextRequest) {
 			return NextResponse.redirect(new URL('/login', req.url))
 		}
 	}
+}
+export const config = {
+	matcher: ['/login', '/profile'],
 }
